10752ab145ff661e663ddc9e10b43f7d619947d9
gmzelias <gmzelias@gmail.com>
2018-08-14 17:59:39 -0400

    test

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -29,13 +29,40 @@ class AuthServiceProvider extends ServiceProvider
         // application. The callback which receives the incoming request instance
         // should return either a User instance or null. You're free to obtain
         // the User instance via an API token or any other method necessary.
+        function getHeaders()
+        {
+        $headers = array();
+        foreach ($_SERVER as $k => $v)
+        {
+        if (substr($k, 0, 5) == "HTTP_")
+        {
+        $k = str_replace('_', ' ', substr($k, 5));
+        $k = str_replace(' ', '-', ucwords(strtolower($k)));
+        $headers[$k] = $v;
+        }
+        }
+        return $headers;    
+        }
+        $cai = getHeaders();
+   
 
-        dd($request);
+        foreach($cai as $x => $x_value) {
+            echo "Key=" . $x . ", Value=" . $x_value;
+            echo "<br>";
+        }
+
+        $cuid = $cai['Api-Token'];
+
+        if ($cuid) {
+            return User::where('api_token', $cuid)->first();
+        }
+
+       /* dd($request);
 
         $this->app['auth']->viaRequest('api', function ($request) {
             if ($request->header('api_token')) {
                 return User::where('api_token', $request->header('api_token'))->first();
             }
-        });
+        });*/
     }
 }

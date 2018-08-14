ac47956c0b20297c31561bc39f613dae92f53236
gmzelias <gmzelias@gmail.com>
2018-08-14 17:18:35 -0400

    Remove Guard

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,8 +31,10 @@ class AuthServiceProvider extends ServiceProvider
         // the User instance via an API token or any other method necessary.
 
         $this->app['auth']->viaRequest('api', function ($request) {
+
+
             echo('first');
-            echo($request->header('api_token'));
+            echo($request);
             if ($request->header('api_token')) {
                 return User::where('api_token', $request->header('api_token'))->first();
             }
